e2d8a4c9c66e30b7382b5ae9a595ccd81562e869
jainabhishek0406@gmail.com <jainabhishek0406@gmail.com>
2019-11-08 02:17:55 +0530

    Java Hello World coomit using Maven build tool.

---
+++ src/main/java/EntryPoint.java
@@ -0,0 +1,13 @@
+package main.java;
+
+/**
+ * @author Abhishek Jain
+ */
+
+public class EntryPoint {
+
+    public static void main(String[] args) {
+		System.out.println("############## First Java Maven Project ##############"); 
+        System.out.println("Hello World");
+    }  
+}
